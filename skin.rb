219d1004584e89b8f1fef13faaae3f512ef9e172
Myke Atkinson <ploe@hotmail.co.uk>
2013-09-25 01:34:41 +0100

    Done!

---
+++ skin.rb
@@ -0,0 +1,33 @@
+#! /usr/bin/ruby
+
+require 'curb'
+
+# Dumb script for downloading all posted images on a thread from 4chan, 
+# and sticks them in their own dir. Multiple URLs can be supplied.
+
+if ARGV.length == 0 then
+	$stderr.puts("Usage: skin [LIST OF URL's...]\nDumps images from the specified 4chan threads in to their own directory\n")
+	exit
+end
+
+ARGV.each { |arg|
+	http = Curl.get(arg)
+	
+	# get the subject of the thread and prepend with Unix timestamp
+	dir = http.body_str.match(/<span class="subject">(.*?)</i).captures[0]
+	dir += Time.now.to_i.to_s
+	Dir.mkdir(dir, 0700)
+	
+	# get every image URL and dump it in to the dir
+	http.body_str.scan(/<a href="\/\/(images.4chan.org\/.\/src\/.*?)"/i) { |i|
+		i = i.join
+		image = Curl.get(i)
+
+		file = dir + "/" + i.sub(/images.4chan.org\/.\/src\//i, "").to_s
+		File.open(file, "wb") { |f|
+			f.write(image.body_str)
+		}
+	}
+}
+
+
